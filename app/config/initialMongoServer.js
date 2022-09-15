aa536cd6f5d47e805dc79950e5610f27b946a34c
Muaaz Bin Saeed <muaaz.binsaeed@hotmail.com>
2022-09-15 17:18:59 +0500

    Remove private keys

--- app/config/initialMongoServer.js
+++ app/config/initialMongoServer.js
@@ -1,7 +1,6 @@
 //Initial Mongo Connection
 const mongoose = require("mongoose");
-let uri = process.env.MONGODB_URL || "mongodb+srv://cluster0-un:cluster0-pw@cluster0.ty583cd.mongodb.net/nest-crud-demo?retryWrites=true&w=majority";
-//"mongodb://localhost:27017/products";
+let uri = process.env.MONGODB_URL || "mongodb://localhost:27017/products";
 mongoose.connect(uri, {
   useNewUrlParser: true,
 });
